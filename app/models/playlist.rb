475a1bd9e99fd8a365c91fbb724c4b004e47331c
kaorusasaki <sasaki.k@opst.co.jp>
2016-06-10 07:23:39 +0000

    date format

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,10 @@ class Playlist < ActiveRecord::Base
   has_many :playlist_contents
   has_many :contents, :through => :playlist_contents
   belongs_to :channel
+  
+    def convert_date
+    self.validity_start_date = self.validity_start_date.gsub('/','')
+    self.validity_end_date = self.validity_end_date.gsub('/','')
+  end
+  
 end
