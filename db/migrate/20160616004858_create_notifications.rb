1c9c4eebea220c9ce89efe0b53d84b9ab2c17e8d
kaorusasaki <sasaki.k@opst.co.jp>
2016-06-16 06:09:19 +0000

    notice modify

--- db/migrate/20160616004858_create_notifications.rb
+++ db/migrate/20160616004858_create_notifications.rb
@@ -2,7 +2,8 @@ class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
       t.string :text
-
+      t.integer :lock_version, default: 0
+      
       t.timestamps null: false
     end
   end
